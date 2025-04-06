ac88d31eef48164e439e872214f18f08b5b291a4
ufimoni <ekongbert02@gmail.com>
2025-04-06 19:19:40 +0100

    v1.0

---
+++ client/src/Redux/loaderSlice.js
@@ -0,0 +1,19 @@
+//////// THIS DASHBOARD WILL DISPLAY USER BASED ON THEIR PROFESSION
+
+/*
+/
+/ const RoleBasedHome = () => {
+  const { user } = useSelector(state => state.userReducer);
+
+  switch (user.role) {
+    case 'farmer':
+      return <FarmerDashboard />;
+    case 'manager':
+      return <ManagerDashboard />;
+    case 'expert':
+      return <ExpertDashboard />;
+    default:
+      return <div>Unknown role</div>;
+  }
+};
+ */
